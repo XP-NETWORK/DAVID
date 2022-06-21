32021bc6ea9e7d70477248a251e529eb0918ff4a
ronyXnes <derrarony@gmail.com>
2022-06-21 18:19:27 +0300

    deploy fix

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,8 +6,11 @@ async function main() {
     console.log("Account ballance:", (await deployer.getBalance()).toString());
   
     const Token = await ethers.getContractFactory("GuiltyDavid");
-    const token = await Token.deploy("Guilty David" , "DAVID",  "https://image.shutterstock.com/image-photo/large-thick-industrial-black-metal-600w-1081708619.jpg",
-    "https://image.shutterstock.com/image-vector/grunge-link-lines-nodes-icon-600w-2080549558.jpg");   
+    const token = await Token.deploy(
+      "Guilty David" , 
+      "DAVID", 
+    "https://image.shutterstock.com/image-photo/large-thick-industrial-black-metal-600w-1081708619/",
+    "https://image.shutterstock.com/image-vector/grunge-link-lines-nodes-icon-600w-2080549558/");   
   
     await token.deployed();
   
